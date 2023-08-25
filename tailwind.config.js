808fc842fbce6a9b9c2b1e557f39fb50c5e342fe
hannesn <nortje.johannes@gmail.com>
2023-08-25 16:26:24 +0200

    Start

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  
+  content: [
+    './src/**/*.{html,js,ts,vue}',
+    
+],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
