6fc3f4902e636c020eaf7c7fcf7313d57862ea4a
hannesn <nortje.johannes@gmail.com>
2023-08-28 20:06:34 +0200

    Fix error with cy type check

--- src/layouts/DefaultLayout.cy.ts
+++
@@ -1,8 +0,0 @@
-import DefaultLayout from './DefaultLayout.vue'
-
-describe('<DefaultLayout />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(DefaultLayout)
-  })
-})
