e14e7ed67ee1e964113fcd51c02194d9c7fbc9e5
hannesn <nortje.johannes@gmail.com>
2023-08-31 16:19:02 +0200

    Mobile map better

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
       <LeafletMap :mapDetails="mapPropDetails" @mapIsVisible="mapIsVisible"></LeafletMap>
     </template>
     <div>
-      <row>
+      <div class="flex flex-col space-y-17">
         <LeafletMap v-if="visible" :mapDetails="mapPropDetails" class="z-50"></LeafletMap>
         <CV @mapDetails="mapDetails" :class="visible ? 'pt-12' : 'pt-0'"></CV>
-      </row>
+      </div>
     </div>
 
     <!-- <nav>
