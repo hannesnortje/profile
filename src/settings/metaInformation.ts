93dc7e5d5e00d3af3dadedd8e0794c7c65c50841
hannesn <nortje.johannes@gmail.com>
2023-08-31 12:56:11 +0200

    Correct Address

--- src/settings/metaInformation.ts
+++ src/settings/metaInformation.ts
@@ -1,7 +1,7 @@
 export const metaInformation = {
     profile_of: "Dr. Johannes A. Nortjé",
     picture: "/johannes-nortjé.png",
-    address: "Am Kindergarten 17, 33165 Lichtenau, Nordrein-Wesfahlen, Germany",
+    address: "Am Kindergarten 19, 33165 Lichtenau, Nordrein-Wesfahlen, Germany",
     email: "hannes.nortje@gmail.com",
     telephone: "+4917680037985"
 }
