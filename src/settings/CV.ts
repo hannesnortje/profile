a0367bccb22d57c7f7a7257723bb2849aacb287a
hannesnortje <45423244+hannesnortje@users.noreply.github.com>
2025-05-28 14:19:11 +0200

    Update CV.ts

--- src/settings/CV.ts
+++ src/settings/CV.ts
@@ -177,7 +177,7 @@ const ceruleancircleHTML = {
   heading: 'Cerulean Circle GmbH',
   description: 'Developing Web4x serverless JavaScript components in ONCE with the WODA IDE. Engineered local AI server hosting offline models, as well as creating several Python AI applications. Creating intelligent AI agents for real-world use cases and provided AI development consulting services. Extensive and proficient Vibe programming',
   startYear: 2024,
-  endYear: ,
+  endYear: -,
   elaboration: [
     'HTML, CSS and JavaScript',
     'JavaScript',
@@ -189,7 +189,7 @@ const ceruleancircleHTML = {
   ],
   mapDetails: {
     marker: [48.143540087423816, 11.575464554035177],
-    name: 'Wittelsbacherplatz 1, 80333 München, Germany'
+    name: 'Wittelsbacherplatz 1, München, Germany'
   }
 };
 
